75b70e43d6437e9e1c4cce3f77ce81ad0b03710d
Scott <poyenchiu26@gmail.com>
2024-10-21 22:52:41 +0800

    Add files via upload

---
+++ Linked List Project.py
@@ -0,0 +1,52 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def print_list(self):
+        cur_node = self.head
+        while cur_node:
+            print(cur_node.data, end=" -> ")
+            cur_node = cur_node.next
+        print("None")
+
+    def delete_node(self, key):
+        cur_node = self.head
+        if cur_node and cur_node.data == key:
+            self.head = cur_node.next
+            cur_node = None
+            return
+        prev = None
+        while cur_node and cur_node.data != key:
+            prev = cur_node
+            cur_node = cur_node.next
+        if cur_node is None:
+            return
+        prev.next = cur_node.next
+        cur_node = None
+
+if __name__ == "__main__":
+    llist = LinkedList()
+    llist.append("A")
+    llist.append("B")
+    llist.append("C")
+    llist.append("D")
+    print("Original list:")
+    llist.print_list()
+    llist.delete_node("B")
+    print("List after deleting 'B':")
+    llist.print_list()
